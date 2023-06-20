b5c56569c2b31812a221f5a5f428fed56828512f
antoniolamorgese <34546111+antoniolamorgese@users.noreply.github.com>
2023-06-20 08:18:04 +0200

    Update language-notice-for-multilanguage-site.php
    plugin update

--- language-notice-for-multilanguage-site.php
+++ language-notice-for-multilanguage-site.php
@@ -107,14 +107,16 @@ if(!is_Admin()) {
 							             var mexInfo = 'This article is also available in your language.';
 							             var mexLink = 'Read the version in your language.';
 									  }	
+									  if (window.location.href.indexOf("category") === -1) {
+										 if (jQuery(window).width() > 768) {
+											// Desktop Device
+											jQuery('#language_notice_for_multilanguage_site_0').html('<table style="border: 0px solid #fff3cd; border-collapse: collapse; text-align: center; vertical-align: middle; margin-top: 2px; margin-bottom: 2px;"><tr style="border: 0px solid #fff3cd; border-collapse: collapse;"><td style="border: 0px solid #fff3cd; border-collapse: collapse;"><i class="fa fa-language" style="font-size: 62px; color: blue;" aria-hidden="true"></i></td><td style="border: 0px solid #fff3cd; border-collapse: collapse;"><p class="has-medium-font-size"><span id="language_notice_for_multilanguage_site_1"><b>Bye!</b></span><br><span id="language_notice_for_multilanguage_site_2">This Post is also available in English.</span><br><a id="language_notice_for_multilanguage_site_3" href="" target="_blank"><span id="language_notice_for_multilanguage_site_4">Read the English version</span></a></p></td><td style="border: 0px solid #fff3cd; border-collapse: collapse;"><a style="float:right;" href="#" class="close" data-dismiss="alert" aria-label="close"><i style="font-size:24px; color:black;" class="fa fa-close" aria-hidden="true"></i></a></td></tr></table>');
+										 } else {
+											// Mobile Device
+											jQuery('#language_notice_for_multilanguage_site_0').html('<a style="float:right;" href="#" class="close" data-dismiss="alert" aria-label="close"><i style="font-size:24px; color:black;" class="fa fa-close" aria-hidden="true"></i></a><i class="fa fa-language" style="font-size: 62px; color: blue;" aria-hidden="true"></i><p class="has-medium-font-size"><span id="language_notice_for_multilanguage_site_1"><b>Bye!</b></span><br><span id="language_notice_for_multilanguage_site_2">This Post is also available in English.</span><br><a id="language_notice_for_multilanguage_site_3" href="" target="_blank"><span id="language_notice_for_multilanguage_site_4">Read the English version</span></a></p>');
+										 }
+									  }	 
 
-									  if (jQuery(window).width() > 768) {
-										 // Desktop Device
-									     jQuery('#language_notice_for_multilanguage_site_0').html('<table style="border: 0px solid #fff3cd; border-collapse: collapse; text-align: center; vertical-align: middle; margin-top: 2px; margin-bottom: 2px;"><tr style="border: 0px solid #fff3cd; border-collapse: collapse;"><td style="border: 0px solid #fff3cd; border-collapse: collapse;"><i class="fa fa-language" style="font-size: 62px; color: blue;" aria-hidden="true"></i></td><td style="border: 0px solid #fff3cd; border-collapse: collapse;"><p class="has-medium-font-size"><span id="language_notice_for_multilanguage_site_1"><b>Bye!</b></span><br><span id="language_notice_for_multilanguage_site_2">This Post is also available in English.</span><br><a id="language_notice_for_multilanguage_site_3" href="" target="_blank"><span id="language_notice_for_multilanguage_site_4">Read the English version</span></a></p></td><td style="border: 0px solid #fff3cd; border-collapse: collapse;"><a style="float:right;" href="#" class="close" data-dismiss="alert" aria-label="close"><i style="font-size:24px; color:black;" class="fa fa-close" aria-hidden="true"></i></a></td></tr></table>');
-									  } else {
-										 // Mobile Device
-										 jQuery('#language_notice_for_multilanguage_site_0').html('<a style="float:right;" href="#" class="close" data-dismiss="alert" aria-label="close"><i style="font-size:24px; color:black;" class="fa fa-close" aria-hidden="true"></i></a><i class="fa fa-language" style="font-size: 62px; color: blue;" aria-hidden="true"></i><p class="has-medium-font-size"><span id="language_notice_for_multilanguage_site_1"><b>Bye!</b></span><br><span id="language_notice_for_multilanguage_site_2">This Post is also available in English.</span><br><a id="language_notice_for_multilanguage_site_3" href="" target="_blank"><span id="language_notice_for_multilanguage_site_4">Read the English version</span></a></p>');
-									  }
 								      jQuery('#language_notice_for_multilanguage_site_3').attr('href', href);
 								      jQuery('#language_notice_for_multilanguage_site_1').replaceWith('<span id="language_notice_for_multilanguage_site_1"><b>'+mexBye+'!</b></span>');
 								      jQuery('#language_notice_for_multilanguage_site_2').replaceWith('<span id="language_notice_for_multilanguage_site_2">'+mexInfo+'</span>');
